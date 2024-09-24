55ab05e9dbe494b3528d4f53ea9df783f191cb39
samyak <samyakmhrzn9841@gmail.com>
2024-09-24 18:18:50 +0545

    url??

--- api/chat-app/src/App.tsx
+++ api/chat-app/src/App.tsx
@@ -6,7 +6,7 @@ import Routes from './assets/Routes.js';
 
 function App() {
 
-  axios.defaults.baseURL = 'http://localhost:4000';
+  axios.defaults.baseURL = 'https://chat-app-live-v2.onrender.com';
   axios.defaults.withCredentials = true;
   
 
